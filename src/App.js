83b1373a0f50d99b71f3c5a7c322a623d5ae2a51
jihye <jiji.pak41@gmail.com>
2022-11-28 00:54:32 +0000

    ✨ Handle onClick event when clicking search button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import { Header } from './components/Header';
 
 function App() {
+  const [search, setSearch] = useState();
+  const onClick = (searchTerm) => {
+    setSearch(searchTerm);
+  };
+
   return (
     <div className="App">
-      <Header />
+      <Header onClick={onClick} />
       <Outlet />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import { MagnifyingGlass } from 'phosphor-react';
+import { useRef } from 'react';
 
-export function Header() {
+export function Header({ onClick }) {
+  const inputRef = useRef();
   return (
     <>
-      {' '}
       <header className="">Youtube</header>
-      <input placeholder="Search..."></input>
-      <button>
+      <input placeholder="Search..." ref={inputRef}></input>
+      <button onClick={() => onClick(inputRef.current.value)}>
         <MagnifyingGlass title="search-icon" />
       </button>
     </>
