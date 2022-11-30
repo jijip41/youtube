a9fc72a5a23d67444bbf09283d3e985141dffaa7
jihye <jiji.pak41@gmail.com>
2022-11-30 22:44:43 +0000

    ➕ Add react query

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import { Header } from './components/Header';
+
+const queryClient = new QueryClient();
 
 function App() {
   const [search, setSearch] = useState();
@@ -12,7 +15,9 @@ function App() {
   return (
     <div className="App">
       <Header onClick={onClick} />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </div>
   );
 }
