36af9eadb119e589d60f2ee2106f83522ac17858
jihye <jiji.pak41@gmail.com>
2022-12-03 22:47:19 +0000

    ✨ Change api to deploy

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,12 +1,10 @@
 import { createContext, useContext } from 'react';
-import { MockYoutubeClient } from '../apis/mockYoutubeClient';
 import { Youtube } from '../apis/youtube';
 import { YoutubeClient } from '../apis/youtubeClient';
 
 export const YoutubeApiContext = createContext();
 
-const client = new MockYoutubeClient();
-// const client = new YoutubeClient();
+const client = new YoutubeClient();
 const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
